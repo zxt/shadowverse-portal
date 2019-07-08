2b7df2468afccfba5bf8730f1addbb64c717f907
Zhao Tang <zxt@zhaotang.ca>
2019-07-08 00:31:05 -0400

    strip trailing whitespace from json values
    was breaking stuff because of not matching names
    because of the extra whitespace. why are there even trailing
    whitespace in the first place...

--- scrape.py
+++ scrape.py
@@ -2,6 +2,28 @@
 import urllib.request
 import urllib.parse
 import json
+import sys
+from pkgutil import simplegeneric
+
+@simplegeneric
+def get_items(obj):
+    while False: # no items, a scalar object
+        yield None
+
+@get_items.register(dict)
+def _(obj):
+    return obj.items() # json object
+
+@get_items.register(list)
+def _(obj):
+    return enumerate(obj) # json array
+
+def strip_whitespace(json_data):
+    for key, value in get_items(json_data):
+        if hasattr(value, 'strip'): # json string
+            json_data[key] = value.strip()
+        else:
+            strip_whitespace(value) # recursive call
 
 def get_cards():
     base_url = 'https://shadowverse-portal.com/api/v1/cards'
@@ -12,6 +34,7 @@ def get_cards():
     url = base_url + '?' + url_params
     with urllib.request.urlopen(url) as response:
         cards_json = json.loads(response.read())
+        strip_whitespace(cards_json)
         with open('json/cards.json', 'w', encoding='utf-8') as outfile:
             json.dump(cards_json['data'], outfile, ensure_ascii=False, indent=2)
 
