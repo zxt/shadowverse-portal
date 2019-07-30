7445bff7487a5e579c6236c5b5a0a304f35ab2d2
Zhao Tang <zxt@zhaotang.ca>
2019-07-30 19:52:56 -0400

    bump package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="svportal",
-    version="0.1",
+    version="0.2",
     author="Zhao Tang",
     author_email="zxt@zhaotang.ca",
     description="Python API wrappers around shadowverse-portal",
