1f19d6f99aa7e21f7bd869e05b6b86ac4c1e4d12
Zhao Tang <zxt@zhaotang.ca>
2019-08-20 20:19:15 -0400

    move lookup tables into separate enums.py file

---
+++ svportal/enums.py
@@ -0,0 +1,49 @@
+CARD_SETS = [(10000, 'Basic'),
+             (10001, 'Standard'),
+             (10002, 'Darkness Evolved'),
+             (10003, 'Rise of Bahamut'),
+             (10004, 'Tempest of the Gods'),
+             (10005, 'Wonderland Dreams'),
+             (10006, 'Starforged Legends'),
+             (10007, 'Chronogenesis'),
+             (10008, 'Dawnbreak,Nightedge'),
+             (10009, 'Brigade of the Sky'),
+             (10010, 'Omen of the Ten'),
+             (10011, 'Altersphere'),
+             (10012, 'Steel Rebellion'),
+             (10013, 'Rebirth of Glory'),
+             (70001, 'Prebuilt Decks Set 1'),
+             (70002, 'Prebuilt Decks Set 2'),
+             (70003, 'Anigera Didoooon Tie-in'),
+             (70004, 'Fate/stay night [Heaven\'s feel] Tie-in'),
+             (70005, 'Prebuilt Decks Set 4'),
+             (70006, 'Prebuilt Decks Set 5'),
+             (70008, 'Princess Connect! Re:Dive Tie-in'),
+             (70009, 'One-Punch Man Tie-in'),
+             (70010, 'Re:ZERO Tie-in'),
+             (70011, 'Prebuilt Decks Set 6'),
+             (90000, 'Tokens')
+            ]
+
+CARD_RARITYS = [(1, 'Bronze'),
+                (2, 'Silver'),
+                (3, 'Gold'),
+                (4, 'Legendary')
+               ]
+
+CARD_TYPES = [(1, 'Follower'),
+              (2, 'Amulet'),
+              (3, 'Countdown Amulet'),
+              (4, 'Spell')
+             ]
+
+CRAFTS = [(0, 'Neutral'),
+          (1, 'Forestcraft'),
+          (2, 'Swordcraft'),
+          (3, 'Runecraft'),
+          (4, 'Dragoncraft'),
+          (5, 'Shadowcraft'),
+          (6, 'Bloodcraft'),
+          (7, 'Havencraft'),
+          (8, 'Portalcraft')
+             ]
--- svportal/scripts/create_db.py
+++ svportal/scripts/create_db.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+import sys
 import os
 import contextlib
 import sqlite3
 import json
+
+sys.path.insert(1, os.path.realpath(os.path.pardir))
+from enums import *
 
 def create_db():
     with contextlib.suppress(FileNotFoundError):
@@ -48,56 +52,6 @@ def create_db():
                         )""".format(t))
 
 def populate_lookup_tables():
-    CARD_SETS = [(10000, 'Basic'),
-                 (10001, 'Standard'),
-                 (10002, 'Darkness Evolved'),
-                 (10003, 'Rise of Bahamut'),
-                 (10004, 'Tempest of the Gods'),
-                 (10005, 'Wonderland Dreams'),
-                 (10006, 'Starforged Legends'),
-                 (10007, 'Chronogenesis'),
-                 (10008, 'Dawnbreak,Nightedge'),
-                 (10009, 'Brigade of the Sky'),
-                 (10010, 'Omen of the Ten'),
-                 (10011, 'Altersphere'),
-                 (10012, 'Steel Rebellion'),
-                 (10013, 'Rebirth of Glory'),
-                 (70001, 'Prebuilt Decks Set 1'),
-                 (70002, 'Prebuilt Decks Set 2'),
-                 (70003, 'Anigera Didoooon Tie-in'),
-                 (70004, 'Fate/stay night [Heaven\'s feel] Tie-in'),
-                 (70005, 'Prebuilt Decks Set 4'),
-                 (70006, 'Prebuilt Decks Set 5'),
-                 (70008, 'Princess Connect! Re:Dive Tie-in'),
-                 (70009, 'One-Punch Man Tie-in'),
-                 (70010, 'Re:ZERO Tie-in'),
-                 (70011, 'Prebuilt Decks Set 6'),
-                 (90000, 'Tokens')
-                ]
-
-    CARD_RARITYS = [(1, 'Bronze'),
-                    (2, 'Silver'),
-                    (3, 'Gold'),
-                    (4, 'Legendary')
-                   ]
-
-    CARD_TYPES = [(1, 'Follower'),
-                  (2, 'Amulet'),
-                  (3, 'Countdown Amulet'),
-                  (4, 'Spell')
-                 ]
-
-    CRAFTS = [(0, 'Neutral'),
-              (1, 'Forestcraft'),
-              (2, 'Swordcraft'),
-              (3, 'Runecraft'),
-              (4, 'Dragoncraft'),
-              (5, 'Shadowcraft'),
-              (6, 'Bloodcraft'),
-              (7, 'Havencraft'),
-              (8, 'Portalcraft')
-             ]
-
     conn = sqlite3.connect('db/cards.db')
     with conn:
         cur = conn.cursor()
