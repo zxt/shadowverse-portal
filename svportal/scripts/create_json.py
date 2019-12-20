3e66e8b6028d700e2157e6e730c8cc2d368350ef
Zhao Tang <zxt@zhaotang.ca>
2019-12-20 17:00:54 -0500

    update create_json script
    update all class specific json files at once by default, instead
    of manually one by one

--- svportal/scripts/create_json.py
+++ svportal/scripts/create_json.py
@@ -14,9 +14,15 @@ def main() :
 
     crafts = ['all', 'forest', 'sword', 'rune', 'dragon', 'shadow', 'blood', 'haven', 'portal']
 
-    cards_json = cards.get_cards(craft)
-    with open('json/'+crafts[craft]+'.json', 'w', encoding='utf-8') as outfile:
-        json.dump(cards_json, outfile, ensure_ascii=False, indent=2)
+    if craft == 0:
+        for i, c in enumerate(crafts):
+            cards_json = cards.get_cards(i)
+            with open('json/'+c+'.json', 'w', encoding='utf-8') as outfile:
+                json.dump(cards_json, outfile, ensure_ascii=False, indent=2)
+    else:
+        cards_json = cards.get_cards(craft)
+        with open('json/'+crafts[craft]+'.json', 'w', encoding='utf-8') as outfile:
+            json.dump(cards_json, outfile, ensure_ascii=False, indent=2)
 
 if __name__ == "__main__":
     main()
