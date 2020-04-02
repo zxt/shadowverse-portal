1598798b6975c47bf251434e35c1b9f03570c3cd
Zhao Tang <zxt@zhaotang.ca>
2020-04-02 10:46:32 -0400

    04.01 update
    - nerfs to Damian, Ironglider, Plessie

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="svportal",
-    version="16.0.1",
+    version="16.0.2",
     license="MIT License",
     author="Zhao Tang",
     author_email="zxt@zhaotang.ca",
