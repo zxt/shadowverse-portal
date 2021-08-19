03b2b6f0aa2db54a028df9ae877596557da6021b
Zhao Tang <zxt@zhaotang.ca>
2021-08-19 10:43:37 -0400

    v3.4.20 update
    - renascent chronicles mini expansion release
    - changes to drache, sentry gate

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="svportal",
-    version="21.0.2",
+    version="21.5",
     license="MIT License",
     author="Zhao Tang",
     author_email="zxt@zhaotang.ca",
