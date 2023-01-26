5bb43af173e87ceef9c11ab4e102068a00aa186a
Zhao Tang <zxt@zhaotang.ca>
2023-01-25 23:25:34 -0500

    v4.0.10 update
    - nerfs to Orichalcum Golem, Atomy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="svportal",
-    version="27.1",
+    version="27.2",
     license="MIT License",
     author="Zhao Tang",
     author_email="zxt@zhaotang.ca",
