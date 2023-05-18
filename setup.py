0ae4e43960fc3bf28c45c2fa74c08b89fad7ca79
Zhao Tang <zxt@zhaotang.ca>
2023-05-17 21:19:50 -0400

    v4.1.21 update
    - Academy of Ages mini expansion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="svportal",
-    version="28.2",
+    version="28.5",
     license="MIT License",
     author="Zhao Tang",
     author_email="zxt@zhaotang.ca",
