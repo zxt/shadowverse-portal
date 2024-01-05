1619b2dbcacf215257c19f40e208c4d127180aa9
Zhao Tang <zxt@zhaotang.ca>
2024-01-04 23:28:41 -0500

    update for jan 4
    - nerf to arcane instruction
    - nerf to scorching curse

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="svportal",
-    version="31.0",
+    version="31.1",
     license="MIT License",
     author="Zhao Tang",
     author_email="zxt@zhaotang.ca",
