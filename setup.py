04fa6f8ce7226aa6bfd4f9689ae1afa7624150ba
Zhao Tang <zxt@zhaotang.ca>
2025-02-17 14:01:14 -0500

    update for February
    - February Throwback Rotation (Fortune's Hand - Renascent Chronicles)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="svportal",
-    version="33.6",
+    version="33.7",
     license="MIT License",
     author="Zhao Tang",
     author_email="zxt@zhaotang.ca",
