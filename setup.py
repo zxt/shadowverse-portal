04c4dd3caced5aebce4824172b99bc5b6609a7d9
Zhao Tang <zxt@zhaotang.ca>
2025-09-29 21:50:35 -0400

    update for September
    - September Throwback Rotation (Omen of the Ten–Verdant Conflict)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="svportal",
-    version="33.10",
+    version="33.13",
     license="MIT License",
     author="Zhao Tang",
     author_email="zxt@zhaotang.ca",
