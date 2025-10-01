fba60616468ed0ddf20363d272b1af1dbbf3b596
Zhao Tang <zxt@zhaotang.ca>
2025-09-30 23:02:25 -0400

    update for October
    - October Throwback Rotation (Ultimate Colosseum–Eternal Awakening)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="svportal",
-    version="33.13",
+    version="33.14",
     license="MIT License",
     author="Zhao Tang",
     author_email="zxt@zhaotang.ca",
