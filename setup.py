c90c3faba965985739276e53db570a75ee897218
Zhao Tang <zxt@zhaotang.ca>
2025-11-02 17:38:01 -0500

    update for November
    - November Throwback Rotation (Brigade of the Sky–Rebirth of Glory)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="svportal",
-    version="33.14",
+    version="33.15",
     license="MIT License",
     author="Zhao Tang",
     author_email="zxt@zhaotang.ca",
